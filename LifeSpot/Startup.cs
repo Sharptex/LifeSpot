b59b10d3e0a16a7e1b77f014a95671d63e9751af
Alex <AleKsusha@DESKTOP-G48TPNV>
2021-11-02 00:48:25 +0300

    SecondCommit

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -36,7 +36,6 @@ namespace LifeSpot
 
                 endpoints.MapGet("/Static/CSS/index.css", async context =>
                 {
-                    // по аналогии со страницей Index настроим на нашем сервере путь до страницы со стилями, чтобы браузер знал, откуда их загружать
                     var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
                     var css = await File.ReadAllTextAsync(cssPath);
                     await context.Response.WriteAsync(css);
